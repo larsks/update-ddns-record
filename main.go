76c995cf74b3eed70a6b2b63b0dd3cdbe259d032
Lars Kellogg-Stedman <lars@oddbit.com>
2022-02-22 08:52:30 -0500

    Be more consistent about return content

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -12,15 +14,66 @@ import (
 	"github.com/aws/aws-sdk-go/service/route53"
 )
 
+type (
+	Response struct {
+		Status   string
+		Message  string
+		Hostname string `json:",omitempty"`
+		Address  string `json:",omitempty"`
+		Result   string `json:",omitempty"`
+	}
+
+	ResponseModifier func(*Response)
+)
+
+func NewResponse(status, message string, mods ...ResponseModifier) *Response {
+	resp := Response{
+		Status:  status,
+		Message: message,
+	}
+
+	for _, mod := range mods {
+		mod(&resp)
+	}
+
+	return &resp
+}
+
+func WithHostInfo(hostname, address string) ResponseModifier {
+	return func(resp *Response) {
+		resp.Hostname = hostname
+		resp.Address = address
+	}
+}
+
+func WithApiResult(res string) ResponseModifier {
+	return func(resp *Response) {
+		resp.Result = res
+	}
+}
+
+func (resp *Response) String() string {
+	text, err := json.Marshal(resp)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(text)
+}
+
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (string, error) {
 	update_token := os.Getenv("DDNS_UPDATE_TOKEN")
 	hosted_zone_id := os.Getenv("DDNS_HOSTED_ZONE_ID")
 
 	if len(update_token) == 0 || len(hosted_zone_id) == 0 {
-		return "Missing required configuration\n", nil
+		return NewResponse("error", "Missing required configuration").String(), nil
 	}
 
 	clientAddress := request.Headers["x-forwarded-for"]
+	if len(clientAddress) == 0 {
+		return NewResponse("error", "Unknown client address").String(), nil
+	}
+
 	hostName := request.QueryStringParameters["hostname"]
 	req_token := request.QueryStringParameters["token"]
 	sess := session.Must(session.NewSession())
@@ -28,7 +81,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 	log.Printf("checking request from %s for %s", clientAddress, hostName)
 	if req_token != update_token {
-		return "Invalid update token\n", nil
+		return NewResponse("error", "Invalid udpate token").String(), nil
 	}
 
 	log.Printf("updating %s in zone %s", hostName, hosted_zone_id)
@@ -56,10 +109,17 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 	resp, err := svc.ChangeResourceRecordSets(&changes)
 	if err != nil {
-		return err.Error(), err
+		return NewResponse(
+			"error", "Failed to update record",
+			WithApiResult(err.Error()),
+		).String(), nil
 	}
 
-	return resp.GoString(), nil
+	return NewResponse("success",
+		fmt.Sprintf("Updated address for %s", hostName),
+		WithHostInfo(hostName, clientAddress),
+		WithApiResult(resp.GoString()),
+	).String(), nil
 }
 
 func main() {
